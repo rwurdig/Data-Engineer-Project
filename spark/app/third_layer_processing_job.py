e4896fcdb1a2564035792dc658e33cff8a1589b1
Rodrigo Soares Wurdig <72509000+rwurdig@users.noreply.github.com>
2023-10-18 01:29:48 -0300

    Update third_layer_processing_job.py

--- spark/app/third_layer_processing_job.py
+++ spark/app/third_layer_processing_job.py
@@ -15,7 +15,7 @@ def main():
     namenode = "hadoop-namenode:8020"
 
     # Main table without transformation
-    trips_df = spark.read.format("parquet").load(f"hdfs://{namenode}/data/bronze/tripdata/")
+    trips_df = spark.read.format("parquet").load(f"hdfs://{namenode}/data/bronze/datatrip/")
 
     # First transformation of the date
     preprocess_data = trips_df.withColumn("datetime", date_format("datetime", "yyyy-MM-dd HH:mm"))
@@ -23,7 +23,7 @@ def main():
     # First task: Grouping trips with similar origin, destination, and time of day
     preprocess_data_time_of_day = preprocess_data.withColumn("time_of_day", date_format("datetime", "HH:mm"))
     grouped_df = preprocess_data_time_of_day.groupBy("region", "origin_coord", "destination_coord", "time_of_day").agg(count("datasource").alias("trips"))
-    write_to_hdfs(grouped_df, f"hdfs://{namenode}/data/silver/tripdata/")
+    write_to_hdfs(grouped_df, f"hdfs://{namenode}/data/silver/datatrip/")
 
     # Second task: Weekly average number of trips for a region
     grouped_df_avg_weeks = preprocess_data.groupBy("region", year("datetime").alias("year"), weekofyear("datetime").alias("week")).agg(count("datasource").alias("trips"))
@@ -32,7 +32,7 @@ def main():
     write_to_hdfs(avg_trips, f"hdfs://{namenode}/data/silver/avg_trips_region/")
 
     # Delivering the raw-data
-    write_to_hdfs(preprocess_data, f"hdfs://{namenode}/data/silver/rawtripdata/")
+    write_to_hdfs(preprocess_data, f"hdfs://{namenode}/data/silver/rawdatatrip/")
 
 if __name__ == "__main__":
     main()
